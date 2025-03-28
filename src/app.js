fd74d86bdfb8572bc981a1534895afb479fc6d1a
Nikhil Chavan <nikhilnchavan@outlook.com>
2025-03-28 10:50:34 +0530

    middleware configration done

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+import cookie from "cookie-parser";
+import cookieParser from "cookie-parser";
 
-const app=express();
+const app = express();
+// enable the json data
+app.use(express.json({ limit: "16kb" }));
 
-export{app}
+// enable the url data
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
+
+// enable the cors
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+
+// enable the cookie
+app.use(cookieParser());
+
+export { app };
